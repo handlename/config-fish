7279c96d44eca3e0472d221583a5a3cf5e705e0f
handlename <nagata@handlena.me>
2019-06-15 16:39:10 +0900

    refactor: use fzf instead of peco
    no reason ;)

--- config.fish
+++ config.fish
@@ -66,7 +66,7 @@ alias g 'hub'
 
 function git-cd
     set -l topdir (git rev-parse --show-toplevel)
-    set -l dir (git ls-files --full-name $topdir | grep '/' | perl -nE 's![^/]+$!!; say' | sort | uniq | peco)
+    set -l dir (git ls-files --full-name $topdir | grep '/' | perl -nE 's![^/]+$!!; say' | sort | uniq | fzf)
     cd "$dir"
 end
 

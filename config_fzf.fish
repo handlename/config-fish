58893c694293bd84947f9ff4feea2e42e1354032
handlename <nagata@handlena.me>
2019-06-16 17:51:43 +0900

    feat: redefine \cf
    I want to use \cf as forward-char, not as __fzf_find_file.

--- config_fzf.fish
+++ config_fzf.fish
@@ -4,4 +4,11 @@ function fzf_git_checkout_branch -d "Fuzzy-find and checkout a branch"
     git branch --all | grep -v HEAD | string trim | fzf | read -l result; and git checkout "$result"
 end
 
+bind -e \cf '__fzf_find_file'
+bind -e -M insert \cf '__fzf_find_file'
+
+if bind -M insert >/dev/null 2>/dev/null
+    bind -M insert \cxf '__fzf_find_file'
+end
+
 bind \cxb 'fzf_git_checkout_branch'

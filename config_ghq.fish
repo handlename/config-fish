54bb80b34183091546d8921e0aa2ed638ff82eb7
handlename <nagata@handlena.me>
2019-06-17 11:58:07 +0900

    feat: unbound \cg

--- config_ghq.fish
+++ config_ghq.fish
@@ -1,2 +1,10 @@
 # keybindings
+
+bind -e \cg '__ghq_repository_search'
+bind -e -M insert \cg '__ghq_repository_search'
+
+if bind -M insert >/dev/null 2>/dev/null
+    bind -M insert \cxg '__ghq_repository_search'
+end
+
 bind \cxg '__ghq_repository_search'

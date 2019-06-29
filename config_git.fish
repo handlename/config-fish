3aebad5c790a382914904dc02f74448ea01b0bf9
handlename <nagata@handlena.me>
2019-06-29 16:27:06 +0900

    style: add newline before pipes
    for readability.

--- config_git.fish
+++ config_git.fish
@@ -4,20 +4,42 @@ alias g 'hub'
 # functions
 function git-cd
     set -l topdir (git rev-parse --show-toplevel)
-    set -l dir (git ls-files --full-name $topdir | grep '/' | perl -nE 's![^/]+$!!; say' | sort | uniq | fzf --preview="ls -la $topdir/{}")
+
+    set -l dir (git ls-files --full-name $topdir \
+    | grep '/' \
+    | perl -nE 's![^/]+$!!; say' \
+    | sort \
+    | uniq \
+    | fzf --preview="ls -la $topdir/{}")
+
     cd "$topdir/$dir"
     commandline --function repaint
 end
 
 function git-checkout-branch
-    set branch (git branch --all | grep -v HEAD | string trim | fzf --no-multi)
-    git checkout (echo "$branch" | sed "s/.* //" | sed "s#remotes/[^/]*/##")
+    set branch (git branch --all \
+    | grep -v HEAD \
+    | string trim \
+    | fzf --no-multi \
+    )
+
+    git checkout (echo "$branch" \
+    | sed "s/.* //" \
+    | sed "s#remotes/[^/]*/##" \
+    )
+
     echo -e '\n'
     commandline -f repaint
 end
 
 function git-help
-    set -l cmd (git help -a | egrep '^  \S' | xargs -n1 echo | sort | fzf --preview 'git help {} | head -20' --bind 'ctrl-e:execute-silent(open dash://manpages:git-{})+abort')
+    set -l cmd (git help -a \
+    | egrep '^  \S' \
+    | xargs -n1 echo \
+    | sort \
+    | fzf --preview 'git help {} | head -20' \
+          --bind    'ctrl-e:execute-silent(open dash://manpages:git-{})+abort' \
+    )
 
     if ! test -n "$cmd"
         commandline --function repaint

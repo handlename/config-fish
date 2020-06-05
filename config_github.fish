0c9c7c6171bc42e91b03edf8f972421a5be4d00a
handlename <nagata@handlena.me>
2020-06-05 18:08:58 +0900

    style: fix indent

--- config_github.fish
+++ config_github.fish
@@ -5,9 +5,9 @@
 
 function github-switch-pr
     set -l pr (gh pr list --state open --limit 100 2>/dev/null \
-        | fzf --preview 'gh pr view (echo {} | cut -f1)' \
-        --bind    'ctrl-e:execute(gh pr view --web (echo {} | cut -f1))' \
-        | cut -f1
+    | fzf --preview 'gh pr view (echo {} | cut -f1)' \
+          --bind    'ctrl-e:execute(gh pr view --web (echo {} | cut -f1))' \
+    | cut -f1
     )
 
     if ! test -n "$pr"

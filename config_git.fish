309bd815f8ac46be5cb6890533bfed9345510109
handlename <nagata@handlena.me>
2020-06-05 18:03:16 +0900

    feat: function github-switch-pr

--- config_git.fish
+++ config_git.fish
@@ -55,6 +55,24 @@ function git-help
     commandline --insert "$cmd "
 end
 
+function github-switch-pr
+    set -l pr (gh pr list --state open --limit 100 2>/dev/null \
+    | fzf --preview 'gh pr view (echo {} | cut -f1)' \
+          --bind    'ctrl-e:execute(gh pr view --web (echo {} | cut -f1))' \
+    | cut -f1
+    )
+
+    if ! test -n "$pr"
+        commandline --function repaint
+        return
+    end
+
+    gh pr checkout "$pr"
+
+    echo -e '\n'
+    commandline --function repaint
+end
+
 function github-open-pr
     gh pr view --web
 end

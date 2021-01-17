4b559f0cc8ac6529505615427fe728765326df8c
handlename <nagata@handlena.me>
2021-01-17 17:49:44 +0900

    feat: use newer curl installed via homebrew

--- config_homebrew.fish
+++ config_homebrew.fish
@@ -1,3 +1,6 @@
 # https://github.com/settings/applications#personal-access-tokens
 # set -x HOMEBREW_GITHUB_API_TOKEN SECRET
 set -x HOMEBREW_NO_ANALYTICS 1
+
+# for curl
+set -x PATH (brew --prefix)/opt/curl/bin $PATH

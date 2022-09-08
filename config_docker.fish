72ee2deb9afc90d8a993dbaaa2dd4f2b54cdc570
handlename <nagata@handlena.me>
2022-09-08 17:16:18 +0900

    feat: function docker-container

--- config_docker.fish
+++ config_docker.fish
@@ -3,4 +3,10 @@ function docker-image
     commandline --insert "$image"
 end
 
+function docker-container
+    set -l id (docker container ls | fzf --header-lines=1 | awk '{print $1}')
+    commandline --insert "$id"
+end
+
 bind \cxdi docker-image
+bind \cxdc docker-container

72065d600a70a7e1cb8420e9e61a9b0b55bf3587
handlename <nagata@handlena.me>
2022-09-13 10:55:08 +0900

    feat: add brew shellenv

--- config_homebrew.fish
+++ config_homebrew.fish
@@ -1,3 +1,5 @@
+eval (/opt/homebrew/bin/brew shellenv)
+
 # https://github.com/settings/applications#personal-access-tokens
 # set -x HOMEBREW_GITHUB_API_TOKEN SECRET
 set -x HOMEBREW_NO_ANALYTICS 1

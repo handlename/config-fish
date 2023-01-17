47c6e7bfe8b7a9ce88ec3dcde9839ad7eb3b5c71
handlename <nagata@handlena.me>
2023-01-17 16:42:21 +0900

    fix: source asdf.fish

--- config_asdf.fish
+++ config_asdf.fish
@@ -1,4 +1,5 @@
 if [ (uname -s) = 'Darwin' ]
+    source (brew --prefix asdf)/libexec/asdf.fish
     source (brew --prefix asdf)/share/fish/vendor_completions.d/asdf.fish
 else
     source $HOME/.asdf/asdf.fish

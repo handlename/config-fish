488472cb3603540b53b1808fcb4d4a0491af1033
handlename <nagata@handlena.me>
2024-03-14 17:20:14 +0900

    fix: only run if asdf installed

--- config_asdf.fish
+++ config_asdf.fish
@@ -1,3 +1,7 @@
+if ! which asdf >/dev/null
+    return
+end
+
 if [ (uname -s) = Darwin ]
     source (brew --prefix asdf)/libexec/asdf.fish
     source (brew --prefix asdf)/share/fish/vendor_completions.d/asdf.fish
